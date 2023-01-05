90e4eae016d6abc57b95674c1980ce03a54a8685
dot-eagle <lionvic3@gmail.com>
2023-01-05 09:25:25 -0800

    Write a function that prints the last digit of a number.

--- 0x01-python-if_else_loops_functions/las_di.py
+++ 0x01-python-if_else_loops_functions/las_di.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import random
-num = random.randint(-10000, 10000)
-#num = int(input("enter the number:"))
+
+# import random
+# num = random.randint(-10000, 10000)
+# num = int(input("enter the number:"))
+
+def print_last_digit(number):
 ldigit = num%10
 print("last digit: {}".format(ldigit))

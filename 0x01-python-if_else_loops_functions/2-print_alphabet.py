150a4757d18f6ae24b2e9a87f44c3d25c5b9adfc
dot-eagle <lionvic3@gmail.com>
2023-01-05 05:22:31 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,6 @@
 Print the ASCII alphabet in lowercase, not followed by a new line.
 """
 for alph in range(97, 123):
-    print(f"{alph:c}", end=" ")
+    print("{:c}".format(alph), end=" ")
+  #  print("{}".format(chr(letter)), end="")
+  #  print(f"{alph:c}", end=" ")

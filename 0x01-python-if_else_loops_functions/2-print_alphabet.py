1712573c631332871e6fd0efc7c0287b9f59d263
dot-eagle <lionvic3@gmail.com>
2023-01-05 05:44:00 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 Print the ASCII alphabet in lowercase, not followed by a new line.
 """
 for alph in range(97, 123):
-    print("{}".format(chr(alph)), end=" ")
+    print("{:c}".format(alph), end="")

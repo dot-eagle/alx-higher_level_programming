4b097681e79cd061bd8e6a48cec8601e0cbc0bcc
dot-eagle <lionvic3@gmail.com>
2023-01-05 06:03:53 -0800

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Print the ASCII alphabet in lowercas except q and ee, 
-not followed by a new line.
+Print the ASCII alphabet in lowercase except q and e,
+not followed by a new linee.
 """
 for alph in range(97, 123):
     if chr(alph) == 'e' or chr(alph) == 'q':

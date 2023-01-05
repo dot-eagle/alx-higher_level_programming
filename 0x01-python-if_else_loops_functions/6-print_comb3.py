9161abe1c0b23f951cb53cb4c9fda78da49b8a02
dot-eagle <lionvic3@gmail.com>
2023-01-05 07:49:57 -0800

    Write a program that prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for numb in range (0, 9):
+    for numb2 in range (numb + 1, 10):
+        if (numb == 8 and numb2 == 9):
+            print(f"{numb:d}{numb2:d}".format(numb, numb2))
+        else:
+            print(f"{numb:d}{numb2:d}".format(numb, numb2), end=", ")

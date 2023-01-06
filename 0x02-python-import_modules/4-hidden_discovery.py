9a99c1b9d54ca517f73b87ce0b5f28b75ff6268f
dot-eagle <lionvic3@gmail.com>
2023-01-06 02:48:26 -0800

    Write a program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import hidden_4
+
+    listofnames = dir(hidden_4)
+    for name in listofnames:
+        if name[:2] != "__":
+            print("{:d}".format(name))

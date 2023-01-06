d3b71d9f58bf8c4074d3ae73cd126df2f37dd135
dot-eagle <lionvic3@gmail.com>
2023-01-05 19:08:08 -0800

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     len_Arg = len(argv)
     cout = abs(len_Arg - 1)
 
-    if (len_Arg == 0):
+    if (cout == 0):
         print("0 argument.")
-    elif (len_Arg == 1):
-        print("{:d} argument.".format(len_Arg))
+    elif (cout == 1):
+        print("{:d} argument.".format(cout))
     else:
-        print("{:d} arguments:".format(len_Arg))
+        print("{:d} arguments:".format(cout))
     for n in range(1, len_Arg):
         print("{:d}: {:s}".format(n, argv[n]))

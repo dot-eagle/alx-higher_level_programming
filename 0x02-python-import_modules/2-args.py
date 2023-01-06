c6501e2545245e4884bcd8aa310c913443364539
dot-eagle <lionvic3@gmail.com>
2023-01-05 17:35:12 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,12 @@
 
 if __name__ == "__main__":
     from sys import argv
-    # lenOfArg = len(argv)
-    cout = len(argv) - 1
+    len_Arg = len(argv)
+    cout = abs(len_Arg - 1)
 
     if (cout == 0):
         print("0 argument.")
-  #  elif (cout == 1):
-     #   print("1 argument.")
     else:
         print("{:d} arguments:".format(cout))
-    for n in range(1, cout+1):
+    for n in range(1, len_Arg):
         print("{:d}: {:s}".format(n, argv[n]))

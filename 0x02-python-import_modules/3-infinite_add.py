402689f216c477258d5b57b4346f9f3f0fa3d8b7
dot-eagle <lionvic3@gmail.com>
2023-01-06 02:25:21 -0800

    sys.argv

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv as ag
-    sum = 0
-    for n in range(1, len(ag)):
-        sum += int(ag[n])
-        print("{:d}".format(sum))
+    import sys
+    result = 0
+    for n in range(1, len(sys.argv)):
+        result += int(sys.argv[n])
+        print("{:d}".format(result))

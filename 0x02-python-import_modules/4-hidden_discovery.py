c1ad7bf81d1d924e8c93abac98635ba64ea07c55
dot-eagle <lionvic3@gmail.com>
2023-01-06 03:25:01 -0800

    4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     listofnames = dir(hidden_4)
     for name in listofnames:
         if name[:2] != "__":
-            print("{:d}".format(name))
+            print("{}".format(name))

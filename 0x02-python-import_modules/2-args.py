54b429e1642008ba5372932d591dd820ea753ad5
dot-eagle <lionvic3@gmail.com>
2023-01-05 16:56:41 -0800

    Write a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 
 if __name__ == "__main__":
     from sys import argv
-    # ilenOfArg = len(argv)
+    # lenOfArg = len(argv)
     cout = len(argv) - 1
 
-    if (cout == 1):
+    if (cout == 0):
         print("0 argument.")
-    elif (cout == 2):
-        print("1 argument:")
+    elif (cout == 1):
+        print("1 argument.")
     else:
         print("{:d} arguments:".format(cout))
     for n in range(1, cout+1):

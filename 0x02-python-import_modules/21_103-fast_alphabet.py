bf4c1e097bb46431a7e89ce99bf49a324426469c
dot-eagle <lionvic3@gmail.com>
2023-01-07 08:07:28 -0800

    Write a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x02-python-import_modules/21_103-fast_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# write a python program to print alphabets in lowercase from a to z
+
+print("\nLowercase Alphabets are:")
+
+for alph in range(97, 123):
+    print(chr(alph), end=" \n")
+    # iprint("\n")

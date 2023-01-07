6741865ccada0ebf195eb0f5c479d72bc5619f87
dot-eagle <lionvic3@gmail.com>
2023-01-07 08:17:54 -0800

    8. ByteCode -> Python3 -Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if (a < b):
+        res = add(a, b)
+        for x in range(4, 6):
+            res = add(res, x)
+        return (res)
+
+    else:
+        return (sub(a, b))

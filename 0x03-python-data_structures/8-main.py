e5b06f5f0b2fd0bc2950cd8f644b94f5a60b34d2
dot-eagle <lionvic3@gmail.com>
2023-01-09 10:04:41 -0800

    8. More returns: Write a function that returns a tuple with the length of a string and its first character.

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -2,7 +2,7 @@
 
 multiple_returns = __import__('8-multiple_returns').multiple_returns
 
-sentence = "At my first school, I first learnt C!"
+sentence = ""
 length, first = multiple_returns(sentence)
 
 print("Length: {:d} - First character: {}".format(length, first))

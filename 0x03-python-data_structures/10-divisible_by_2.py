a50f0ff403c191978fc6ed79e7c1da3064272d10
dot-eagle <lionvic3@gmail.com>
2023-01-09 16:37:26 -0800

    10-main.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,12 +6,12 @@ def divisible_by_2(my_list=[]):
 
     if (not my_list):
         return (my_list)
-
-    for check in my_list:
-        if (check % 2 == 0):
-            new_list[check] = True
-        else:
-            new_list[check] = False
+    else:
+        for check in my_list:
+            if (check % 2 == 0):
+                new_list[check] = True
+            else:
+                new_list[check] = False
     return (new_list)
 
 

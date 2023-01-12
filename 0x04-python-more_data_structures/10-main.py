6b238cbcfdcc7a251e950b32916debf0f6655e72
dot-eagle <lionvic3@gmail.com>
2023-01-12 01:29:50 -0800

    retu

--- 0x04-python-more_data_structures/10-main.py
+++ 0x04-python-more_data_structures/10-main.py
@@ -2,6 +2,7 @@
 
 best_score = __import__('10-best_score').best_score
 
+# a_dictionary = {}
 a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
 best_key = best_score(a_dictionary)
 print("Best score: {}".format(best_key))

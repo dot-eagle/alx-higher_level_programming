b66e0066e727433ba097489b486d7b85e3323b61
dot-eagle <lionvic3@gmail.com>
2023-01-12 10:23:11 -0800

    klt

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,7 +10,7 @@ def roman_to_int(roman_string):
                 "C": 100, "D": 500,
                 "M": 1000
                 }
-        for ele in roman_string [::-1]: # 2
+        for ele in roman_string[::-1]:      # 2
             val = roman_map[ele]
             if sol < val * 5:
                 sol += val
@@ -21,7 +21,5 @@ def roman_to_int(roman_string):
         return (0)
 
 
-    # roman_dict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,
-     #       'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
-
-
+# roman_dict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,
+# 'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}

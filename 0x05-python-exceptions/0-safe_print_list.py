15c09e44d252c3356e8a7de9991c529150e7dc3a
dot-eagle <lionvic3@gmail.com>
2023-01-23 20:48:45 -0800

    0. Safe list printing: Write a function that prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    while True:
+        try:
+            if my_list is None:
+                return (0)
+
+            idx = 0
+
+            for ele in range(x):
+                print("{}".format(my_list[ele]), end='')
+                idx = idx + 1
+
+        except IndexError:
+            pass
+            #print("INVALID INDEX")
+
+        finally:
+            print()
+
+        return (idx)

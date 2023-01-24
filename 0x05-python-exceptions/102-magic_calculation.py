e9ea0d9b3327e2851384ef4f69fba1bb3e24fa07
dot-eagle <lionvic3@gmail.com>
2023-01-24 10:41:37 -0800

    9. ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
-def safe_function(fct, *args):
+
+def magic_calculation(a, b):
     sol = 0
 
-    for s in range(1, 3):
-        try:
-            if (s > a):
-                raise Exception("Too far")
-            else:
-                sol += (a**b) / s
-        except Exception:
-            sol = a + b
-            break
+    while True:
+        for s in range(1, 3):
+            try:
+                if (s > a):
+                    raise Exception("Too far")
+                else:
+                    sol += (a ** b) / s
+            except Exception:
+                sol = a + b
+                break
         return (sol)

41bc438dcdcb4a817e840f27a89b4918d4c9736b
dot-eagle <lionvic3@gmail.com>
2023-01-24 09:32:51 -0800

    8. Safe function : Write a function that executes a function safely.

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    while True:
+        try:
+            return (fct(*args))
+
+        except Exception as err:
+            sys.stderr.write("Exception: {}\n".format(err))
+
+        return None

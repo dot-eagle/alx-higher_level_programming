5390405631e4d8f55ee34bc36e951cb1336d0bba
dot-eagle <lionvic3@gmail.com>
2023-01-24 14:20:42 -0800

    1. Square with size: Write a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,21 @@
 
 """
 Next step in Square Class
+Module 1-square
+Defines class Square with private attribute size
 """
 
 
 class Square:
+    """
+    class Square definition
+    Args:
+        size : size of a side in square
+    """
     def __init__(self, size):
+        """
+        Initializes square
+        Attributes:
+            size: size of a side of square
+        """
         self.__size = size

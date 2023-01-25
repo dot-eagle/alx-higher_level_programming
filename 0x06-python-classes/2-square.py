6c7f7e8f3c6b3ff1aeeeac3b7732f74262315faa
dot-eagle <lionvic3@gmail.com>
2023-01-25 15:17:24 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 
-# Module 2-square: Defines class Square with private attribute size and validates size
-
+"""
+Module 2-square
+Defines class Square with private attribute size and validates size
+"""
 
 
 class Square:
-    def __init__(self, size=0):  # init allow square class to be used
-        self.__size = size   # asign private instance attribute size
+    def __init__(self, size=0): 
+    """ 
+    init allow square class to be used
+    """
+        self.__size = size
+        """
+        asign private instance attribute size
+        """
 
         if type(size) is not int:
             raise TypeError("size must be an integer")

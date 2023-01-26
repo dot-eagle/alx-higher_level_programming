15e921adbe29c427e5481343cc69055f23502388
dot-eagle <lionvic3@gmail.com>
2023-01-25 20:06:32 -0800

    value[1] < 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,10 +56,10 @@ class Square:
         if type(value) is not tuple or len(value) != 2:
             self.__position = None
             raise TypeError(posta)
-        elif type(value[0]) is not int or type(value[1]) is not int:
+        elif type(value[0]) is not int or value[1] < 0:
             self.__position = None
             raise TypeError(posta)
-        elif value[0] < 0 or value[1] < 0:
+        elif value[0] < 0 or type(value[1]) is not int:
             self.__position = None
             raise TypeError(posta)
         else:
@@ -106,4 +106,4 @@ class Square:
             for rows in range(self.__size):
                 print("{}{}".format(" " * self.position[0], "#" * self.__size))
             if self.__size == 0:
-                print("")
+                print()

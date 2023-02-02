bc5145232b126f508e2772583106f840aae15f92
dot-eagle <lionvic3@gmail.com>
2023-02-02 04:32:02 -0800

    30. Low memory cost : Write a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,7 +11,8 @@ class LockedClass:
     can't set Except for first_name """
 
     def __setattr__(self, attribute, value):
+        dmsg = ("'LockedClass' object has no attribute '")
         if attribute == "first_name":
             self.__dict__[attribute] = value
         else:
-            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
+            raise AttributeError(dmsg + attribute + "'")

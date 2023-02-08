030c88d85d4141cc152df590ebcd116582456d15
dot-eagle <lionvic3@gmail.com>
2023-02-08 00:46:29 -0800

    8. Class to JSON: Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Python Module for JSON serialization of an object
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """ Function that returns the dictionary description with simple
+        data structure for JSON serialization of an object
+        Args: 
+            obj - a serializable instance / attribute of the Class """
+
+    return (obj.__dict__)

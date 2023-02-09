ca2d4e3d56f19613fac7c18c4bdf38d1f431be66
dot-eagle <lionvic3@gmail.com>
2023-02-09 08:12:01 -0800

    print({}.format(sorted(self)))

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,16 @@ Python class MyList that inherits frm list
 
 class MyList(list):
     """ class MyList that inherits frm list """
+
+    '''def __init__(self, list):
+        """ def __init__(self, *args, **kwargs):
+        Initialize and construct MyList instatnces """
+        super(list).__init__(*args, **kwargs)'''
+
     def print_sorted(self):
         """ prints the list, but in ascending sort order """
-        print(sorted(self))
+        """ super(list).sorted() """
+        '''print(sorted(super(list)))
+        print(self) '''
+        print("{}".format(sorted(self)))
+

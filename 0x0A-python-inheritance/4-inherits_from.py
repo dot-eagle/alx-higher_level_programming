721afc2d208331b91f869c63bd061c7e672ae1ec
dot-eagle <lionvic3@gmail.com>
2023-02-09 09:27:06 -0800

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,6 +18,6 @@ def inherits_from(obj, a_class):
         return (False)
 
     if type(obj) == a_class:
-        return (False)
-    else:
         return (True)
+    else:
+        return (False)

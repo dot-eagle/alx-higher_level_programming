029665a84bee5afefe6d970bba1db67568354d71
dot-eagle <lionvic3@gmail.com>
2023-02-09 11:22:22 -0800

    6. Improve Geometry : Write a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Python module of class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """ Function of a class BaseGeometry 
+     (based on 5-base_geometry.py) """
+
+    def area(self):
+        """ area: Public instance method """
+        raise Exception("area() is not implemented")

64e664c3e3408a065a3fa2ca7e8edbc269b8ae1d
dot-eagle <lionvic3@gmail.com>
2023-02-09 17:49:27 -0800

    13. Can I?: Write a function that adds a new attribute to an object if it’s possible

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Python module that adds a new attribute
+"""
+
+
+def add_attribute(a_class, name, value):
+    """ Adds new attribute to an object if it's possible """
+
+    # Set for O(1) membership test
+    attr_list = {int, str, float, list, dict, tuple, frozenset, type, object}
+
+    if type(a_class) in attr_list:
+        raise TypeError("can't add new attribute")
+
+    a_class.__setattr__(name, value)
+

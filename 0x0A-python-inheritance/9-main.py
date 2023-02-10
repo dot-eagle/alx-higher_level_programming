fbc0e65bdf2e6f7c91a7b24d8e13e299a983991b
dot-eagle <lionvic3@gmail.com>
2023-02-09 16:10:20 -0800

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-main.py
@@ -0,0 +1,7 @@
+ectangle = __import__('9-rectangle').Rectangle
+
+r = Rectangle(3, 5)
+
+print(r)
+
+print(r.area())
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+Python module of class Rectangle that inherits from (7-base_geometry.py)
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Function of a class Rectangle that inherits from BaseGeometry
+    (based on 7-base_geometry.py) """
+
+    def __init__(self, width, height):
+        """ Instantiation with private instances - width and height """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ area: Public instance method
+        to compute and return area of Rectangle """
+
+        area = self.__width * self.__height
+        return (area)
+
+    def __str__(self):
+        """ Magic method to return string description of Rectangle """
+
+        str_rep = "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return (str_rep)
+
+    ''' raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Public instance method that validates value """
+        self.name = name
+        self.value = value
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
+            '''

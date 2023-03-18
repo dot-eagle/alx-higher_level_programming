b338345581bdc1043fafceb58aa7244e5e0406d4
dot-eagle <lionvic3@gmail.com>
2023-03-18 10:59:13 -0700

    15-groups.sql: Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- Displays the number of records with the same score in the table second_table
 SELECT `score`, COUNT( * ) AS number FROM `second_table`
-GROUP BY number DESC
+GROUP BY `score`
+ORDER BY number DESC;
 

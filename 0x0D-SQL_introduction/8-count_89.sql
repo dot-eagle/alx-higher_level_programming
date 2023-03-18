b90827b9c7db033be41016dc52602e4cb8ab617e
dot-eagle <lionvic3@gmail.com>
2023-03-18 07:47:29 -0700

    Write a script that creates a table second_table in the database hbtn_0c_0

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,5 @@
--- Show the number of records with id = 89 in the table first_table
-SELECT COUNT( * ) FROM `frst_table`
+-- Show the number of records with id = 89 in the table "first_table"
+SELECT COUNT( * ) FROM `first_table`
 WHERE id=89
 ENGINE=InnoDB;
 

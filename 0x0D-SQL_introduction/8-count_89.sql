6424b19528d28e2f1c6c85b4794a3d790ee3a8bb
dot-eagle <lionvic3@gmail.com>
2023-03-18 07:37:38 -0700

    8. Count 89: Write a script that displays the number of records with id = 89 in the table first_table

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,5 @@
--- Show the number of records with id = 89 in the table first_table of the database hbtn_0c_0
-SELECT COUNT (*) FROM first_table
-WHERE id=89;
+-- Show the number of records with id = 89 in the table first_table
+SELECT COUNT( * ) FROM `frst_table`
+WHERE id=89
+ENGINE=InnoDB;
 

7d2e1c4aaccaedf6900e62cd0ff97804cb48099c
dot-eagle <lionvic3@gmail.com>
2023-03-18 11:15:10 -0700

    16. Say my name: Write a script that lists all records of the table second_table of the database hbtn_0c_0

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- Lists all records of the table second_table of the database hbtn_0c_0
-SELECT `score`, `name` ( * ) FROM `second_table`
+SELECT `score`, `name` FROM `second_table`
 WHERE `name` != NULL
 ORDER BY `score` DESC;
 
